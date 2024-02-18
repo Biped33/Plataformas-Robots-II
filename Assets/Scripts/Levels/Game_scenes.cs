fe54278468d105b9b6b6e8b5e6fe730412635dd0
MinusUserz <ioaihthg7@gmail.com>
2024-02-18 14:17:20 -0600

    Level 2
    Agregar el nivel 2, y todos los sprites, se trató de implementar una cinematica y nuevas escenas para ambos, corregir el error de las plataformas, no puedes bajar por ellas

---
+++ Assets/Scripts/Levels/Game_scenes.cs
@@ -0,0 +1,24 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverS : MonoBehaviour
+
+
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Player has left the gridd");
+    }
+    public void Menu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
