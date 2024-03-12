8e3f3a56c5867d957c2429fb3b28eb737ac9a1ba
MinusUserz <ioaihthg7@gmail.com>
2024-03-12 16:35:55 -0600

    Minor tweaks
    Try to add crash animation.
    Camera adjust and physics.

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour{
-    private int t_movSpeed = 500, t_jumpCount = 2, t_layerint = 3; 
-    private float t_inputHorizontal, t_jumpForce = 35f, t_maxJumpForce = 37f;
+    private int t_movSpeed = 400, t_jumpCount = 2, t_layerint = 3; 
+    private float t_inputHorizontal, t_jumpForce = 30f, t_maxJumpForce = 35f;
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     private Animator animator;
